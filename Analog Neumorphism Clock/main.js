bb33fec5862cbcb15cde7dfa2bdf1733c5ccc961
Saratu Aye Doma <saratudoma@gmail.com>
2023-12-22 00:07:46 +0100

    cleared error

--- Analog Neumorphism Clock/main.js
+++ Analog Neumorphism Clock/main.js
@@ -1,15 +1,17 @@
-const deg = 6;
+const degree = 6;
 const hr = document.querySelector('#hr');
 const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
 
-setInterval(() =>{
-let day = new Date();
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * deg;
-let ss = day.getSeconds() * deg;
+setInterval(() => {
 
-hr.style.transform = 'rotateZ(${hh+(mm/12)}deg)';
-mn.style.transform = 'rotateZ(${mm}deg)';
-sc.style.transform = 'rotateZ(${ss}deg)';
-})
+    const date = new Date();
+    const hh = date.getHours() * 30;
+    const mm = date.getMinutes() * degree;
+    const ss = date.getSeconds() * degree;
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+
+});
